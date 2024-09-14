f342912dc7312188d733e1853c0b367e77c8cdf6
shimulhosen <shimulhosen.cse@gmail.com>
2024-09-14 15:03:11 +0600

    first commit

---
+++ PPP1/Data/PPP1Context.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PPP1.Model;
+
+namespace PPP1.Data
+{
+    public class PPP1Context(DbContextOptions<PPP1Context> options) : DbContext(options)
+    {
+        public DbSet<PPP1.Model.Department> Department { get; set; } = default!;
+    }
+}
---
+++ PPP1/Model/Department.cs
@@ -0,0 +1,8 @@
+﻿namespace PPP1.Model
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+    }
+}
